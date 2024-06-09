97ea6201b50dcfb7844588ff5f5c32ea722854e6
Tristan <64777943+Tristroux@users.noreply.github.com>
2024-06-10 00:00:18 +0200

    Configure and create basic link for classroom with firebase (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
@@ -13,8 +17,13 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
+  //start the firebase link
+  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform);
+
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
   runApp(MyApp(settingsController: settingsController));
+
+
 }
--- lib/src/Model/dataController.dart
+++ lib/src/Model/dataController.dart
@@ -1,104 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:computer_status_reporter/firebase_options.dart';
+import 'package:computer_status_reporter/src/api/classroomRequests.dart';
+
 import 'classroom.dart';
 import 'computer.dart';
 import 'report.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 class DataController {
+
+  final FirebaseFirestore firestore;
+  late final ClassroomRequests classroomRequests;
+
+  DataController({required this.firestore}) {
+    classroomRequests = ClassroomRequests(firestore: firestore);
+  }
+
   // Fonction pour créer un objet Classroom
-  Classroom createClassroom(int id, int classroomNumber, int floor) {
+  Classroom createClassroom(String id, int classroomNumber, int floor) {
     return Classroom(id: id, classroomNumber: classroomNumber, floor: floor);
   }
 
   // Fonction pour retourner une liste d'objets Classroom
-  List<Classroom> createClassroomsList(int count) {
-    return List.generate(count, (index) => Classroom(id: index, classroomNumber: 100 + index, floor: index % 3));
+  Future<List<Classroom>> createClassroomsList() async {
+    return classroomRequests.getClassrooms();
   }
 
   // Fonction pour créer un objet Computer
-  Computer createComputer(int id, String computerName, int classroomId) {
+  Computer createComputer(String id, String computerName, int classroomId) {
     return Computer(id: id, computerName: computerName, classroomId: classroomId);
   }
 
   // Fonction pour retourner une liste d'objets Computer
   List<Computer> createComputersList(int count) {
-    return List.generate(count, (index) => Computer(id: index, computerName: 'Computer $index', classroomId: 100 + index));
+    return List.generate(count, (index) => Computer(id: "index", computerName: 'Computer $index', classroomId: 100 + index));
   }
 
-  // Fonction pour créer un objet Report
-  Report createReport(int id, DateTime creationDate, int classroomId, int computerId, String reportDescription,
-                      bool hdmiIsOk, bool etherIsOk, bool keyboardIsOk, bool mouseIsOk, bool powerIsOk,
-                      bool screenIsOk, bool computerIsOk) {
-    return Report(
-      id: id,
-      creationDate: creationDate,
-      classroomId: classroomId,
-      computerId: computerId,
-      reportDescription: reportDescription,
-      hdmiIsOk: hdmiIsOk,
-      etherIsOk: etherIsOk,
-      keyboardIsOk: keyboardIsOk,
-      mouseIsOk: mouseIsOk,
-      powerIsOk: powerIsOk,
-      screenIsOk: screenIsOk,
-      computerIsOk: computerIsOk,
-    );
-  }
-
-  // Fonction pour retourner une liste d'objets Report
-  List<Report> createReportsList(int count) {
-    return List.generate(count, (index) => Report(
-      id: index,
-      creationDate: DateTime.now().add(Duration(days: index)),
-      classroomId: 100 + index,
-      computerId: 200 + index,
-      reportDescription: 'Report $index',
-      hdmiIsOk: index % 2 == 0,
-      etherIsOk: index % 2 == 0,
-      keyboardIsOk: index % 2 == 0,
-      mouseIsOk: index % 2 == 0,
-      powerIsOk: index % 2 == 0,
-      screenIsOk: index % 2 == 0,
-      computerIsOk: index % 2 == 0,
-    ));
-  }
 }
 
-void main() {
-  DataController dataController = DataController();
+Future<void> main() async {
 
-  // Exemple de création d'un objet Classroom
-  Classroom classroom = dataController.createClassroom(1, 101, 1);
-  print(classroom);
+  
+  //start the firebase link
+  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  DataController dataController = DataController(firestore: firestore);
+  //end link and start app under
 
   // Exemple de création d'une liste d'objets Classroom
-  List<Classroom> classrooms = dataController.createClassroomsList(5);
+  List<Classroom> classrooms = await dataController.createClassroomsList();
+
   classrooms.forEach(print);
-
-  // Exemple de création d'un objet Computer
-  Computer computer = dataController.createComputer(1, 'Dell XPS', 101);
-  print(computer);
-
-  // Exemple de création d'une liste d'objets Computer
-  List<Computer> computers = dataController.createComputersList(5);
-  computers.forEach(print);
-
-  // Exemple de création d'un objet Report
-  Report report = dataController.createReport(
-    1,
-    DateTime.now(),
-    101,
-    202,
-    'Initial check report',
-    true,
-    true,
-    true,
-    true,
-    true,
-    true,
-    true,
-  );
-  print(report);
-
-  // Exemple de création d'une liste d'objets Report
-  List<Report> reports = dataController.createReportsList(5);
-  reports.forEach(print);
+  
 }

405a48bc4b96af78bd55a45526f88f780c8d229b
Tristan <64777943+Tristroux@users.noreply.github.com>
2024-06-10 11:54:34 +0200

    Creation of Report DB + fixing default value for each Classes (#6)

--- lib/src/Model/report.dart
+++ lib/src/Model/report.dart
@@ -1,9 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Report {
   String id;
-  DateTime creationDate;
-  int classroomId;
-  int computerId;
+  Timestamp creationDate;
+  String classroomId;
+  String computerId;
   String reportDescription;
   bool hdmiIsOk;
   bool etherIsOk;
@@ -28,6 +29,39 @@ class Report {
     required this.computerIsOk,
   });
 
+  factory Report.fromMap(Map<String, dynamic> data, String documentId) {
+    return Report(
+      id: documentId,
+      creationDate: data['creationDate'] ?? Timestamp.now(),
+      classroomId: data['classroomId'] ?? "0000",
+      computerId: data['computerId'] ?? "0000",
+      reportDescription: data['reportDescription'] ?? "no desc",
+      hdmiIsOk: data['hdmiIsOk'] ?? false,
+      etherIsOk: data['etherIsOk'] ?? false,
+      keyboardIsOk: data['keyboardIsOk'] ?? false,
+      mouseIsOk: data['mouseIsOk'] ?? false,
+      powerIsOk: data['powerIsOk'] ?? false,
+      screenIsOk: data['screenIsOk'] ?? false,
+      computerIsOk: data['computerIsOk'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'creationDate': creationDate,
+      'classroomId': classroomId,
+      'computerId': computerId,
+      'reportDescription': reportDescription,
+      'hdmiIsOk': hdmiIsOk,
+      'etherIsOk': etherIsOk,
+      'keyboardIsOk': keyboardIsOk,
+      'mouseIsOk': mouseIsOk,
+      'powerIsOk': powerIsOk,
+      'screenIsOk': screenIsOk,
+      'computerIsOk': computerIsOk,
+    };
+  }
+
   @override
   String toString() {
     return 'Report(id: $id, creationDate: $creationDate, classroomId: $classroomId, computerId: $computerId, '
@@ -35,4 +69,3 @@ class Report {
            'mouseIsOk: $mouseIsOk, powerIsOk: $powerIsOk, screenIsOk: $screenIsOk, computerIsOk: $computerIsOk)';
   }
 }
-

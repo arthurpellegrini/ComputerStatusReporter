3a9c8f6bc1d8954e320788265e559b7606a535a2
Tristan <tristan.lemenach@gmail.com>
2024-06-12 21:54:14 +0200

    adding attribute in computer

--- lib/src/model/computer.dart
+++ lib/src/model/computer.dart
@@ -2,11 +2,13 @@ class Computer {
   String id;
   String computerName;
   String classroomId;
+  List<int> positions = [];
 
   Computer({
     required this.id,
     required this.computerName,
     required this.classroomId,
+    required this.positions,
   });
 
   factory Computer.fromMap(Map<String, dynamic> data, String documentId) {
@@ -14,12 +16,24 @@ class Computer {
       id: documentId,
       computerName: data['computerName'] ?? "00-00-00",
       classroomId: data['classroomId'] ?? "0000",
+      positions: [data['positionX'], data['positionY'] ],
     );
+  }
+
+  
+  Map<String, dynamic> toMap() {
+    print(positions);
+    return {
+      'computerName': computerName,
+      'classroomId': classroomId,
+      'positionX': positions[0],
+      'positionY': positions[1],
+    };
   }
 
   @override
   String toString() {
-    return 'Computer(id: $id, computerName: $computerName, classroomId: $classroomId)';
+    return 'Computer(id: $id, computerName: $computerName, classroomId: $classroomId, $positions)';
   }
 }
 

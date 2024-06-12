c63b3a200e5d2f69b4e1d9e194489e8f91f28c2f
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2024-06-12 17:21:57 +0200

    feat: Dark mode with switch

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -21,47 +21,69 @@ class SettingsView extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            DropdownButton<ThemeMode>(
-              value: controller.themeMode,
-              onChanged: controller.updateThemeMode,
-              items: const [
-                DropdownMenuItem(
-                  value: ThemeMode.system,
-                  child: Text('System Theme'),
-                ),
-                DropdownMenuItem(
-                  value: ThemeMode.light,
-                  child: Text('Light Theme'),
-                ),
-                DropdownMenuItem(
-                  value: ThemeMode.dark,
-                  child: Text('Dark Theme'),
+            // DropdownButton<ThemeMode>(
+            //   value: controller.themeMode,
+            //   onChanged: controller.updateThemeMode,
+            //   items: const [
+            //     DropdownMenuItem(
+            //       value: ThemeMode.system,
+            //       child: Text('System Theme'),
+            //     ),
+            //     DropdownMenuItem(
+            //       value: ThemeMode.light,
+            //       child: Text('Light Theme'),
+            //     ),
+            //     DropdownMenuItem(
+            //       value: ThemeMode.dark,
+            //       child: Text('Dark Theme'),
+            //     ),
+            //   ],
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Dark Mode', style: TextStyle(fontSize: 20)),
+                const SizedBox(width: 8),
+                Switch(
+                  value: controller.themeMode == ThemeMode.dark || (controller.themeMode == ThemeMode.system && MediaQuery.of(context).platformBrightness == Brightness.dark),
+                  onChanged: (bool value) {
+                    controller.updateThemeMode(
+                      value ? ThemeMode.dark : ThemeMode.light,
+                    );
+                  },
                 ),
               ],
             ),
-            DropdownButton<Locale>(
-              value: controller.currentLocale,
-              onChanged: (Locale? newLocale) {
-                if (newLocale != null) {
-                  controller.updateLocale(newLocale);
-                }
-              },
-              items: AppLanguages.languages.map((AppLanguage language) {
-                return DropdownMenuItem<Locale>(
-                  value: language.locale,
-                  child: Row(
-                    children: [
-                      CountryFlag.fromLanguageCode(
-                        language.countryCode,
-                        height: 20,
-                        width: 30,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                const Text('Language', style: TextStyle(fontSize: 20)),
+                const SizedBox(width: 8),
+                DropdownButton<Locale>(
+                  value: controller.currentLocale,
+                  onChanged: (Locale? newLocale) {
+                    if (newLocale != null) {
+                      controller.updateLocale(newLocale);
+                    }
+                  },
+                  items: AppLanguages.languages.map((AppLanguage language) {
+                    return DropdownMenuItem<Locale>(
+                      value: language.locale,
+                      child: Row(
+                        children: [
+                          CountryFlag.fromLanguageCode(
+                            language.countryCode,
+                            height: 20,
+                            width: 30,
+                          ),
+                          const SizedBox(width: 8),
+                          Text(language.name),
+                        ],
                       ),
-                      const SizedBox(width: 8),
-                      Text(language.name),
-                    ],
-                  ),
-                );
-              }).toList(),
+                    );
+                  }).toList(),
+                ),
+                ],
             ),
           ],
         ),
